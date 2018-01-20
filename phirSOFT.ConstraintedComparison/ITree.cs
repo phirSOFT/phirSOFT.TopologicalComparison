97f255409e96442c3df6c8fc58eac26eff999a50
Philemon Eichin <phileicht@gmx.de>
2018-01-20 13:46:31 +0100

    Updated Tree interfaces

--- phirSOFT.ConstraintedComparison/ITree.cs
+++ phirSOFT.ConstraintedComparison/ITree.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace phirSOFT.TopologicalComparison
@@ -18,11 +19,17 @@ namespace phirSOFT.TopologicalComparison
     {
         T Value { get; set; }
         IEnumerable<ITreeNode<T>> Children { get; }
+        ITreeNode<T> AddChild(T node);
+        ITreeNode<T> Insert(T node, IEnumerable<ITreeNode<T>> sucessors);
+        ITreeNode<T> Insert(T node, Predicate<ITreeNode<T>> sucessors);
+        ITreeNode<T> MoveChild(ITreeNode<T> child, ITreeNode<T> newParent);
     }
 
     public interface IInverseTreeNode<T>
     {
         T Value { get; set; }
         IInverseTreeNode<T> Parent { get; }
+        IInverseTreeNode<T> InserBefore(T node);
+        void MoveBehind(IInverseTreeNode<T> node);
     }
 }
