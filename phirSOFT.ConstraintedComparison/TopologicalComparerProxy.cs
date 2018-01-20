909e95479a3bb27bc320ae012ae2cf98720d81ab
Philemon Eichin <phileicht@gmx.de>
2018-01-20 13:47:00 +0100

    Add TopologicalComparerProxy

---
+++ phirSOFT.ConstraintedComparison/TopologicalComparerProxy.cs
@@ -0,0 +1,29 @@
+﻿namespace phirSOFT.TopologicalComparison
+{
+    public static class TopologicalComparerProxy
+    {
+        public static bool TryCompare(this ITopologicalComparer comparer, object lhs, object rhs, out int result)
+        {
+            if (comparer.CanCompare(lhs, rhs))
+            {
+                result = comparer.Compare(lhs, rhs);
+                return true;
+            }
+
+            result = 0;
+            return false;
+        }
+
+        public static bool TryCompare<T>(this ITopologicalComparer<T> comparer, T lhs, T rhs, out int result)
+        {
+            if (comparer.CanCompare(lhs, rhs))
+            {
+                result = comparer.Compare(lhs, rhs);
+                return true;
+            }
+
+            result = 0;
+            return false;
+        }
+    }
+}
