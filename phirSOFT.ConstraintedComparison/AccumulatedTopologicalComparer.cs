0df1f9fef74059bddd33617833527b2bf2e97e55
Philemon Eichin <phileicht@gmx.de>
2018-01-22 17:04:23 +0100

    Add AccumulatedTopologicalComparer

---
+++ phirSOFT.ConstraintedComparison/AccumulatedTopologicalComparer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+
+namespace phirSOFT.TopologicalComparison
+{
+    public class  AccumulatedTopologicalComparer<T> : Collection<ITopologicalComparer<T>>, ITopologicalComparer<T>
+    {
+        public int Compare(T x, T y)
+        {
+            return this.First(item => item.CanCompare(x, y)).Compare(x, y);
+        }
+
+        public bool CanCompare(T x, T y)
+        {
+            return this.Any(item => item.CanCompare(x, y));
+        }
+    }
+}
