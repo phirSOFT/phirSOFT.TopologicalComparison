cce8a4c21097b4a8d1be9f54b4e87b707c057620
peic <phileicht@gmx.de>
2018-03-01 13:20:10 +0100

    Add documentation on ITopologicalComparer

--- phirSOFT.ConstraintedComparison/ITopologicalComparer.cs
+++ phirSOFT.ConstraintedComparison/ITopologicalComparer.cs
@@ -11,11 +11,30 @@ namespace phirSOFT.TopologicalComparison
     /// <typeparam name="T">The type to compare</typeparam>
     public interface ITopologicalComparer<in T> : IComparer<T>
     {
+        /// <summary>
+        ///     Determines wheter <paramref name="x"/> and <paramref name="y"/>
+        ///     can be compared.
+        /// </summary>
+        /// <param name="x"> The first operand to compare.</param>
+        /// <param name="y"> The second operand to compare.</param>
+        /// <returns> True, if the two objects can be compared. </returns>
         bool CanCompare(T x, T y);
     }
 
+    /// <inheritdoc />
+    /// <summary>
+    ///     Abstracts the concept of an <see cref="T:System.Collections.IComparer" />
+    ///     in a way, that a comparer is allowed to fail to compare two instances.
+    /// </summary>
     public interface ITopologicalComparer : IComparer
     {
+        /// <summary>
+        ///     Determines wheter <paramref name="x"/> and <paramref name="y"/>
+        ///     can be compared.
+        /// </summary>
+        /// <param name="x"> The first operand to compare.</param>
+        /// <param name="y"> The second operand to compare.</param>
+        /// <returns> True, if the two objects can be compared. </returns>
         bool CanCompare(object x, object y);
     }
 }
