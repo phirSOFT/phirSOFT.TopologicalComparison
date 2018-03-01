2a2ca54ac65155ed91c94431385ce1f16a7531db
peic <phileicht@gmx.de>
2018-03-01 12:53:28 +0100

    Implement InvalidStateException
    InvalidStateException now derives from InvalidOperationException
    
    Implement propper patterns

--- phirSOFT.ConstraintedComparison/InvalidTreeStateException.cs
+++ phirSOFT.ConstraintedComparison/InvalidTreeStateException.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace phirSOFT.TopologicalComparison
 {
-    public class InvalidTreeStateException : Exception
+    public class InvalidTreeStateException : InvalidOperationException
     {
+        public InvalidTreeStateException()
+        {
+        }
+
+        public InvalidTreeStateException(string message) : base(message)
+        {
+        ]
+
+        public InvalidTreeStateException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public InvalidTreeStateException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
