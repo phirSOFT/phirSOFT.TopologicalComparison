2b3ab57b8d1a972d19d848fee779a90d4867d5ca
peic <phileicht@gmx.de>
2018-03-01 12:45:19 +0100

    Add documentation on InvalidTreeStateException

--- phirSOFT.ConstraintedComparison/InvalidTreeStateException.cs
+++ phirSOFT.ConstraintedComparison/InvalidTreeStateException.cs
@@ -2,6 +2,9 @@
 
 namespace phirSOFT.TopologicalComparison
 {
+    /// <summary>
+    ///     This exception is thrown, when a tree is in an invalid state.
+    /// </summary>
     public class InvalidTreeStateException : Exception
     {
     }
