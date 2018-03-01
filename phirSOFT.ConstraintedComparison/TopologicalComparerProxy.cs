4c598c41df1908d09116d7b8498c347b755c07fc
peic <phileicht@gmx.de>
2018-03-01 14:27:36 +0100

    Add documentation on TopologicalComparerProxy

--- phirSOFT.ConstraintedComparison/TopologicalComparerProxy.cs
+++ phirSOFT.ConstraintedComparison/TopologicalComparerProxy.cs
@@ -1,7 +1,25 @@
 ﻿namespace phirSOFT.TopologicalComparison
 {
+    /// <summary>
+    ///     Provides extension method for the <see cref="ITopologicalComparer"/> and 
+    ///     <see cref="ITopologicalComparer{T}"/> interfaces.
+    /// </summary>
     public static class TopologicalComparerProxy
     {
+        /// <summary>
+        ///     Tries to compare two objects using an <see
+        ///     cref="ITopologicalComparer/">.
+        /// </summary>
+        /// <param name="comparer"> The comparer to use.</param>
+        /// <param name="lhs">The first object to compare.</param>
+        /// <param name="rhs">The second object to compare.</param>
+        /// <param name="result">
+        ///     The result of the comparison (if sucessfull). Otherwise the
+        ///     result is set to 0.
+        /// </param>
+        /// <returns>
+        ///     True, if the comparison suecced.
+        /// </returns>
         public static bool TryCompare(this ITopologicalComparer comparer, object lhs, object rhs, out int result)
         {
             if (comparer.CanCompare(lhs, rhs))
@@ -14,6 +32,20 @@
             return false;
         }
 
+        /// <summary>
+        ///     Tries to compare two objects using an <see
+        ///     cref="ITopologicalComparer{T}/">.
+        /// </summary>
+        /// <param name="comparer"> The comparer to use.</param>
+        /// <param name="lhs">The first object to compare.</param>
+        /// <param name="rhs">The second object to compare.</param>
+        /// <param name="result">
+        ///     The result of the comparison (if sucessfull). Otherwise the
+        ///     result is set to 0.
+        /// </param>
+        /// <returns>
+        ///     True, if the comparison suecced.
+        /// </returns>
         public static bool TryCompare<T>(this ITopologicalComparer<T> comparer, T lhs, T rhs, out int result)
         {
             if (comparer.CanCompare(lhs, rhs))
