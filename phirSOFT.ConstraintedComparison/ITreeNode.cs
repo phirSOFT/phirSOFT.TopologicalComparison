9840a6a4498f00f790f9a5e4c93893c4d18294c2
peic <phileicht@gmx.de>
2018-03-01 13:40:36 +0100

    Add documentation to ITreeNode

--- phirSOFT.ConstraintedComparison/ITreeNode.cs
+++ phirSOFT.ConstraintedComparison/ITreeNode.cs
@@ -2,12 +2,48 @@
 
 namespace phirSOFT.TopologicalComparison
 {
+    /// <summary>
+    ///     Provides an interface for a node in an <see cref="ITree{T}"/>
+    /// </summary>
+    /// <typeparam name="T">The type of the value stored in the node. </typeparam>
     public interface ITreeNode<T>
     {
+        /// <summary>
+        ///     Gets or sets value of the node.
+        /// </summary>
         T Value { get; set; }
+
+        /// <summary>
+        ///     Gets all children of this node.
+        /// </summary>
         IEnumerable<ITreeNode<T>> Children { get; }
+
+        /// <summary>
+        ///     Adds a child to this node.
+        /// </summary>
+        /// <param name="node">The value of the node to attach</param>
+        /// <returns>The new added <see cref="ITreeNode{T}"/>.<returns>
         ITreeNode<T> AddChild(T node);
+
+        /// <summary>
+        ///     Detaches an <see cref="ITreeNode{T}"/> from this node.
+        /// </summary>
+        /// <param name="child"/> The node to detach </param>
+        /// <remarks>
+        ///     The child node has to be an direct child of this node.
+        /// </remarks>
         void Detach(ITreeNode<T> child);
+
+        /// <summary>
+        ///     Attaches an <see cref="ITreeNode{T}"/> to this node.
+        /// </summary>
+        /// <param name="child">
+        ///     The <see cref="ITreeNode{T}"/> to attach.
+        /// </param>
+        /// <remarks>
+        ///     The <paramref name="child"/> should not be attached to
+        ///     an other parent.
+        /// </remarks>
         void Attach(ITreeNode<T> child);
     }
 }
