7cd458151af44cc0546f7328a8f20b6de20e9d6e
Philemon Eichin <phileicht@gmx.de>
2018-03-16 15:05:24 +0100

    Add specific trees
    Add interfaces for trees with more spefific node types.
    These are:
    	- ITree<TValue, TNode> where TNode : ITreeNode<TValue, TNode>
    	- ITreeNode<TValue, TNode> where TNode : ITreeNode<TValue,
    	TNode where TNode : ITreeNode<TValue, TNode>>

---
+++ phirSOFT.ConstraintedComparison/ITree`2.cs
@@ -0,0 +1,14 @@
+﻿namespace phirSOFT.TopologicalComparison
+{
+    /// <summary>
+    /// Provides a tree datastructure with a more specific node type.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the values stored in the nodes.</typeparam>
+    /// <typeparam name="TNode">The type of the nodes</typeparam>
+    public interface ITree<TValue, out TNode> : ITree<TValue> where TNode : ITreeNode<TValue, TNode>
+    {
+        /// <inheritdoc cref="ITree{T}.Root"/>
+        new TNode Root { get; }
+    }
+
+}
