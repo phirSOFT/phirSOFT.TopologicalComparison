838ed31579668a25c06c50fe8d9896b399ffd33f
Philemon Eichin <phileicht@gmx.de>
2018-03-16 14:09:43 +0100

    Implement Type comparer

---
+++ phirSOFT.ConstraintedComparison/TypeComparer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace phirSOFT.TopologicalComparison
+{
+    /// <summary>
+    /// Compares types based on ther assignability. A type A is considerd less than type B, if the A is an ancestor of B in the type hirachy. 
+    /// </summary>
+    public class TypeComparer : ITopologicalComparer<Type>, ITopologicalComparer
+    {
+        public int Compare(Type x, Type y)
+        {
+            // Todo: cache results
+            var infoX = x.GetTypeInfo();
+            var infoY = x.GetTypeInfo();
+
+            return (infoY.IsAssignableFrom(infoX) ? 1 : 0) - (infoX.IsAssignableFrom(infoY) ? 1 : 0);
+        }
+
+        public bool CanCompare(Type x, Type y)
+        {
+            // Todo: cache results
+            var infoX = x.GetTypeInfo();
+            var infoY = x.GetTypeInfo();
+
+            return infoY.IsAssignableFrom(infoX) || infoX.IsAssignableFrom(infoY);
+        }
+
+        public int Compare(object x, object y)
+        {
+            return Compare((Type)x, (Type)y);
+        }
+
+        public bool CanCompare(object x, object y)
+        {
+            return x is Type typeX && y is Type typeY && CanCompare(typeX, typeY);
+        }
+    }
+}
