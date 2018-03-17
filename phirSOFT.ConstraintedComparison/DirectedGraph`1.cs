71cae70ca6fad5f6c9e34205fb972a62b98d2122
Philemon Eichin <phileicht@gmx.de>
2018-03-17 14:24:06 +0100

    Implement DirectedGraph

---
+++ phirSOFT.ConstraintedComparison/DirectedGraph`1.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace phirSOFT.TopologicalComparison
+{
+    public class DirectedGraph<T> : IDirectedGraph<T>
+    {
+        private readonly DirectedGraphNode<T> _root;
+
+        public DirectedGraph(T rootValue)
+        {
+            _root = new DirectedGraphNode<T>(rootValue);
+        }
+
+        ITreeNode<T> ITree<T>.Root => _root;
+
+        public IDirectedGraphNode<T> Root => _root;
+
+    }
+}
