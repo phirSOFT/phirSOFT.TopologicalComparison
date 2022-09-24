aff44ab98a7a7cbd61e198bcdc3e25b17ebd3c7e
Philemon Eichin <philemon.eichin@t4dt.com>
2022-09-24 15:13:29 +0200

    Update test project to sdk project

--- phirSOFT.ConstraintedComparisonTests/Properties/AssemblyInfo.cs
+++ phirSOFT.ConstraintedComparisonTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("phirSOFT.ConstraintedComparisonTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("phirSOFT.ConstraintedComparisonTests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("f5366b0e-8248-4cc0-9ba0-f7f7d3309657")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
